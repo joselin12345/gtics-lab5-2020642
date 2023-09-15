d870b980da1bde4750f9d455f146c407cc6f0007
joselin <a20206442@pucp.edu.pe>
2023-09-14 20:24:11 -0500

    proyecto base

---
+++ lab5_20206442/src/test/java/com/example/lab5_20206442/Lab520206442ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20206442;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520206442ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
